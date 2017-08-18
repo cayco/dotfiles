2facb7c22057e6e342472c1392c32c4512df9d10
Krzysztof Kajkowski <krzysztofkajkowski@Krzysztofs-Air.dhcp.intra.codilime.com>
2017-08-18 10:48:08 +0200

    macOs fixes and glue

--- mutt/findmail.sh
+++ mutt/findmail.sh
@@ -2,12 +2,14 @@
 
 MESSAGE=$(cat)
 
-NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
+NEWALIAS=$(echo "${MESSAGE}" | grep -m 1 ^"From: " | sed s/[\,\"\'\<\>]//g | awk '{$1=""; if (NF == 3) {print $0;} else if (NF == 2) {print $0 $0;} else if (NF > 3) {print $0;}}')
+
 
 if grep -Fxq "$NEWALIAS" $HOME/.mutt/aliases.txt; then
     :
 else
     echo "$NEWALIAS" >> $HOME/.mutt/aliases.txt
+    ~/.mutt/addcontact $NEWALIAS 
 fi
 
 echo "${MESSAGE}"
